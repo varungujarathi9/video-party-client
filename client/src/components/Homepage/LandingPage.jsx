128b9cba8af2ef43cabbfb88f22b45b90ece5017
aravind289 <aravindvitkrishnan@gmail.com>
2020-11-17 23:49:32 +0530

    Still working on scoket connection

--- client/src/components/Homepage/LandingPage.jsx
+++ client/src/components/Homepage/LandingPage.jsx
@@ -1,27 +1,50 @@
-import React,{useContext}from 'react'
+import React,{useContext,useEffect, useState}from 'react'
 import {navigate} from '@reach/router'
 import {userContext} from '../helper/usercontext'
+import io from 'socket.io-client'
 
 
+let endpoint = "http://127.0.0.1:5000"
+let socket=io.connect(`${endpoint}`)
 
 export default function LandingPage (){
-    
-    
+
+   
+
+
+
+
+    // const [messages, setMessages] = useState(["Hello And Welcome"]);
+    // const [message, setMessage] = useState("");
+  
+    //const [msg,setMsg] = useState([])
     const user = useContext(userContext)
+
+
+    useEffect(()=>{
+        printUsername()
+        
+    })
+
     const onSubmitForm = (e)=>{
         
         var usernameValid = user.username
-        console.log(Boolean(usernameValid===''))
+        
+        console.log(Boolean(usernameValid ===''))
         if(usernameValid !== ''){
             console.log("check here")
-            navigate("decideroom",{replace:true})
+
+            
+            socket.emit("message", usernameValid);
+            // if(socket.on("username")){
+            //     navigate("decideroom",{replace:true})
+            // }
+            
             e.preventDefault()            
         }
         else{
            e.preventDefault()
-           user.setErrorMsg("please provide username")
-           
-           
+           user.setErrorMsg("please provide username")   
             
         }
 
@@ -35,10 +58,32 @@ export default function LandingPage (){
         
     }
 
+    const printUsername = ()=>{
+        console.log("jhello")
+        socket.on("message",(msg)=>{          
+            console.log("check username msg",msg)
+        })
+    }
+
+    // const displayUsername =() =>{
+    //  socket.on("message",msg=>{
+    //         user.setUsername(msg)
+    //         return user.username
+    //     })
+    // }
+
+    // const welcomeMsgDisplay =(socket)=>{
+    //     // let msg= socket
+       
+
+    // }
+
     
         return(
             <div>
                 <form >
+                    {/* <p>{msg}</p> */}
+                   
                     <label>Username</label>
                     <input type="text" onChange = {handleInput} ></input>
                     <button  onClick={onSubmitForm}>submit</button>
@@ -46,6 +91,7 @@ export default function LandingPage (){
                 <h6 style={{ color: 'red', fontSize: '16px', margin: '5px' }}>
                     {user.errormsg}
                   </h6>
+       
             </div>
         )
     
--- client/src/components/RoomDecidepage/DecideRoom.jsx
+++ client/src/components/RoomDecidepage/DecideRoom.jsx
@@ -1,33 +1,22 @@
 import { navigate } from '@reach/router'
 import React,{useContext} from 'react'
 import {userContext} from '../helper/usercontext'
+import io from 'socket.io-client'
+
 
 
 export default function DecideRoom (){    
     const user = useContext(userContext)
-    const changeRoom=(roomName)=>{
-        if(roomName === 'createroom'){
-            
-            navigate('/createroom')
-        }
-        if(roomName==='joinroom'){
-           
-            navigate('/joinroom')
-        }
-
-        if(roomName==='logout'){
-            
-            navigate('/',{replace:true})
-        }
-        
-    }    
+    
         return(
             <div>
-                <h1>Helo {user.username}</h1>
+                {/* <h1>Helo {socket.on("username",function(){
+                    return user.username
+                })}</h1> */}
                
-                <button  onClick={()=>changeRoom('createroom')}>Create Room</button>
-                <button  onClick={()=>changeRoom('joinroom')}>Join Room</button>
-                <button  onClick={()=>changeRoom('logout')}>Logout</button>
+                <button  onClick={()=>navigate('/createroom')}>Create Room</button>
+                <button  onClick={()=>navigate('/joinroom')}>Join Room</button>
+                <button  onClick={()=>navigate('/',{replace:true})}>Logout</button>
             </div>
         )
     
