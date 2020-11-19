807fb6c8e3d5ab3d5752550087eab7b70b76a366
gcp-server <v.gujarathi777@gmail.com>
2020-11-19 13:09:00 +0000

    fix 4

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. {process.env.REACT_APP_SERVER_TYPE}
+          Edit <code>src/App.js</code> and save to reload. App Stage: {process.env.REACT_APP_SERVER_TYPE}
         </p>
         <a
           className="App-link"
