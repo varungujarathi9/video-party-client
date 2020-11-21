0caabfd54d8620a9e0f17ba83ba7f25166668216
gcp-server <v.gujarathi777@gmail.com>
2020-11-21 06:12:42 +0000

    pseduo commit

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Env Type: {data.ENVIRONMENT_TYPE}
+          Explain. Edit <code>src/App.js</code> and save to reload. Env Type: {data.ENVIRONMENT_TYPE}
         </p>
         <a
           className="App-link"
