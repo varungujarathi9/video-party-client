314e935e555df38db6a885fc26415754c3b406d5
gcp-server <v.gujarathi777@gmail.com>
2020-11-21 19:20:20 +0000

    dynamically taking server url from configs file

--- client/src/components/helper/socketfile.js
+++ client/src/components/helper/socketfile.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
-
-let endpoint="http://127.0.0.1:5000"
-const  socket  = io.connect(`${endpoint}`,{transports: ['websocket'],upgrade:false})
+import data from '../../configs.json'
+let endpoint="http://35.223.15.12:5000"
+const  socket  = io.connect(`${data.SERVER}`,{transports: ['websocket'],upgrade:false})
 
 export {socket}
