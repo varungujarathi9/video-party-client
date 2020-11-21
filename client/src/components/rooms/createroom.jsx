b6332b3072bd788be9aee71869c3ff525bef4dc5
aravind289 <aravindvitkrishnan@gmail.com>
2020-11-22 00:48:44 +0530

    joinee names

--- client/src/components/rooms/createroom.jsx
+++ client/src/components/rooms/createroom.jsx
@@ -2,84 +2,95 @@ import { navigate } from '@reach/router'
 import React from 'react'
 import { socket } from '../helper/socketfile'
 
-export default class CreateRoom extends React.Component{
-    state={
-        filename:'',
-        extension:["mp4","mkv","mpv","avi","webm","x-msvideo","x-matroska"],
-        extensionCheck:false,
-        errorMsg:'',
-        joineeName:[]
-        
-    }
-    
+export default class CreateRoom extends React.Component {
+    state = {
+        filename: '',
+        extension: ["mp4", "mkv", "mpv", "avi", "webm", "x-msvideo", "x-matroska"],
+        extensionCheck: false,
+        errorMsg: '',
+        uNameJoinee: []
 
-    componentDidMount (){
-        socket.on('newJoinee',(joineeName)=>{
-            console.log(joineeName)
-            
-            
+    }
+
+
+    componentDidMount() {
+        socket.on('newJoinee', (joineeName) => {
+            console.log(joineeName.data)
+            this.setState({
+                uNameJoinee: [...this.state.uNameJoinee, joineeName.data]
+            })
+
         })
     }
 
 
-    handleFile=(e)=>{
+    handleFile = (e) => {
         e.preventDefault()
         var filelist = document.getElementById('videofile').files[0]
         console.log(filelist)
         var typeOfFile = filelist.type
         console.log(typeOfFile)
-        var file= e.target.value.replace(/^.*[\\]/, '')
-       
+        var file = e.target.value.replace(/^.*[\\]/, '')
+
         this.setState({
-            filename:file,
-            
+            filename: file,
+
         })
 
         var extensionVal = typeOfFile.split('/')
         console.log(extensionVal)
-        
-        if(this.state.extension.includes(extensionVal[1])){
+
+        if (this.state.extension.includes(extensionVal[1])) {
             this.setState({
-                extensionCheck:true,
-                errorMsg:''
+                extensionCheck: true,
+                errorMsg: ''
             })
         }
-        else{
+        else {
             this.setState({
-                errorMsg:"please provide valid file",
-                extensionCheck:false
+                errorMsg: "please provide valid file",
+                extensionCheck: false
             })
-        } 
+        }
 
         var fileUrl = URL.createObjectURL(filelist).split()
-        localStorage.setItem('video_file',fileUrl)
-        
+        localStorage.setItem('video_file', fileUrl)
 
-    }   
 
-    
-    render(){
-        return(
+    }
+
+
+    render() {
+        const { uNameJoinee } = this.state
+        return (
             <div>
                 <label>Browse file</label>
-                <input type="file"  id="videofile"onChange={this.handleFile}/>
+                <input type="file" id="videofile" onChange={this.handleFile} />
                 <p>{localStorage.getItem('roomId')}</p>
+                {uNameJoinee.length > 0 &&
+                    uNameJoinee.map(item => {
+                        return (
+                            <h3>{item}</h3>
+                        )
+                    })}
+
+
                 <div style={{ fontSize: '16px', margin: '5px' }}>
-                {this.state.extensionCheck ? 
-                <div>
-                <h6 style={{color:'green'}}
-                
-                >{this.state.filename}</h6>
-                
-                    <button onClick={()=>{navigate('/videopage')}}>Next</button>
+                    {this.state.extensionCheck ?
+                        <div>
+                            <h6 style={{ color: 'green' }}
 
+                            >{this.state.filename}</h6>
+
+                            <button onClick={() => { navigate('/videopage') }}>Next</button>
+
+                        </div>
+
+                        : <h6 style={{ color: 'red' }}>{this.state.errorMsg}</h6>}
                 </div>
-                
-                :<h6 style={{color:'red'}}>{this.state.errorMsg}</h6>}
-                  </div>
 
-                <button onClick={()=>{navigate(-1)}}>Back</button>
-        
+                <button onClick={() => { navigate(-1) }}>Back</button>
+
             </div>
         )
     }
