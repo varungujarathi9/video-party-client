9d2fdb0477120c04b57ebaa54ad490b0eef3a419
Varun Gujarathi <v.gujarathi777@gmail.com>
2021-01-08 12:56:55 +0530

    join page created

--- src/components/helper/router.jsx
+++ src/components/helper/router.jsx
@@ -3,6 +3,7 @@ import {Router} from '@reach/router'
 import Home from '../Home'
 import Login from '../Login'
 import Lobby from '../Lobby'
+import Join from '../Join'
 import VideoPlayer from '../VideoPlayer'
 
 export default class RouterPage extends React.Component{
@@ -12,6 +13,7 @@ export default class RouterPage extends React.Component{
                 <Home exact path="/" />
                 <Login exact path="/login" />
                 <Lobby exact path="/lobby" />
+                <Join exact path="/join" />
                 <VideoPlayer exact path="/video-player" />
             </Router>
         )
