193ed89e5fc3cba868032b7f49581c79e9dd87e4
aravind289 <aravindvitkrishnan@gmail.com>
2021-01-09 19:48:02 +0530

    Front page responsive done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,41 +23,41 @@ export default class Home extends React.Component {
     render() {
 
         return (
-            <div>
+            <div className="container-fluid">
                 <div>
                     <h1 className={style.title}>Video Party!!</h1>
                 </div>
-                {/* <p className={style.about}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500
-            </p> */}
-                <div className={style.left}>
-                    
+               
+               {/* create party details */}
+                <div className={`row ${style.btnsdiv}`}>
+                    <div className={`col-md-6 ${style.leftDiv}`}>  
+                    <div  className="col-md-12" style={{maxWidth:"50%",margin:"auto"}}>
                     <button className={style.createBtn} onClick={this.navigateToLogin('creator')}>
                         <div className={style.buttonDiv}>
                             <img className={style.buttonImg} src={CreateRoomPng} alt="createroomimg" />
                             <p className={style.buttonText}>Create Party</p>
                         </div>
                     </button>
-                   
-                    
+                    </div>
                     <div className={style.details}>
                         <h3>Host your own room</h3>
                         <p className={style.btnDetails}>
                             Want to watch movies with your friends?? Host your own room and watch movies with your friends seemlessly.
                     </p>
-                    </div>
-
+                    </div> 
                 </div>
-                <div className={style.right}>
-                    
+
+
+                {/* join btn details */}
+                <div className="col-md-6">
+                <div  className="col-md-12" style={{maxWidth:"50%",margin:"auto"}}>
                     <button className={style.joinBtn} onClick={this.navigateToLogin('joinee')}>
                         <div className={style.buttonDiv}>
                             <img className={style.buttonImg} src={JoinRoomPng} alt="joinroomimg" />
                             <p className={style.buttonText}>Join Party</p>
                         </div>
                     </button>
-                    
-                   
+                    </div>  
                     <div className={style.details}>
                         <h3>Join an existing room</h3>
                         <p className={style.btnDetails}>
@@ -66,8 +66,11 @@ export default class Home extends React.Component {
                     </div>
 
                 </div>
+                </div>
+                <div className={style.developersLink}>
                 <p className={style.extras}>Made with <img className={style.heartImg} src={HeartIcon} alt="heart image" onClick={this.navigatePage} style={{cursor:"pointer"}}/> </p>
-            </div>
+                </div>
+                  </div>
         )
     }
 }
