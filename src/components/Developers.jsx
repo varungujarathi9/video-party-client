d010b0cb4c1e416ee0a176655684e35f37569a6b
Varun Gujarathi <v.gujarathi777@gmail.com>
2021-01-11 03:53:43 +0530

    developers done

--- src/components/Developers.jsx
+++ src/components/Developers.jsx
@@ -20,75 +20,86 @@ export default class DevelperPage extends React.Component {
     render() {
         return (
             <div className="container-fluid">
-                <button className={style.backBtn} onClick={this.navigateBack}>
-                    <div className={style.btnDiv}>                       
+                <div className="col-md-12">
+                    <button className={style.backBtn} onClick={this.navigateBack}>
+                        <div className={style.btnDiv}>
                             <img className={style.btnImg} src={BackIcon} alt="backIcon" />
-                        
-                        <p className={style.btnText}> Back</p>
-                    </div>
-                </button>
-
-                <div className={`row noMargin ${style.developerdiv}`}>
-                    <div className={`col-md-12 ${style.developerdetails}`} >
-                        Developers
-                    </div>
-                    <div className="card" style={{ width: "300px",marginRight:"5%" }}>
-                       
-                        <img src={Ak} className="card-img-top" alt="..." />
-                        
-                        <div className="card-body text-center">
-                            <h5 className="card-title">Aravind Krishnan</h5>
-                            <a href="https://www.linkedin.com/in/aravind-krishnan-808a85152/" target="_blank">
-                            <img src={LinkedIn} className={style.linkImg} alt="linkedin image" />
-                            </a>
+                            <p className={style.btnText}> Back</p>
                         </div>
-                    </div>
-                    <div className="card" style={{ width: "300px" ,marginRight:"5%"}}>
-                        <img src={Varun} className="card-img-top" alt="..." />
-                        <div className="card-body text-center">
-                            <h5 className="card-title">Varun Gujarathi </h5>
-                            <a href="https://www.linkedin.com/in/varungujarathi/" target="_blank">
-                            <img src={LinkedIn} className={style.linkImg} alt="linkedin image" />
-                            </a>
-                        </div>
-                    </div>
-                    <div className="card" style={{ width: "300px",marginRight:"5%" }}>
-                        <img src={Jay} className="card-img-top" alt="..." />
-                        <div className="card-body text-center">
-                            <h5 className="card-title">Jay Khatri</h5>
-                            <a href="https://www.linkedin.com/in/jay-khatri-a52483169/" target="_blank">
-                            <img src={LinkedIn} className={style.linkImg} alt="linkedin image" />
-                            </a>
-                        </div>
-                    </div>
-                    
-                    <div className={`col-md-12 ${style.designerdetails}`}>
-                        Designers
-                    </div>
-                  
-                    <div className="card" style={{ width: "300px",marginRight:"5%" }}>
-                        <img src={Rahul} className="card-img-top" alt="..." />
-                        <div className="card-body text-center">
-                            <h5 className="card-title" >Rahul Bhide</h5>
-                            <a href="https://www.linkedin.com/in/rahul-bhide/" target="_blank">
-                            <img src={LinkedIn} className={style.linkImg} alt="linkedin image" />
-                            </a>
-                        </div>
-                    </div>
-                    <div className="card" style={{ width: "300px",marginRight:"5%" }}>
-                        <img src={Rihan} className="card-img-top" alt="..." />
-                        <div className="card-body text-center">
-                            <h5 className="card-title">Rihan Krithik Ramesh</h5>     
-                            <a href="https://www.linkedin.com/in/rihankrithik/" target="_blank">                       
-                            <img src={LinkedIn} className={style.linkImg} alt="linkedin image" />
-                            </a>
-                        </div>
-                    </div>
-
-                    
-                    
+                    </button>
                 </div>
 
+                <div className={`row noMargin ${style.developersDiv}`}>
+                    <div className={`col-md-12 ${style.developerTitle}`} >
+                        <h1><span>Developers</span></h1>
+                    </div>
+                    <div className="col-md-4 col-sm-12">
+                    <div className={`card ${style.profileCard}`}>
+                            <img src={Varun} className={`card-img-top ${style.developerImage}`} alt="..." />
+                            <div className="card-body text-center">
+                                <h5 className="card-title">Varun Gujarathi</h5>
+                                <a href="https://www.linkedin.com/in/varungujarathi/" target="_blank">
+                                <img src={LinkedIn} className={style.linkImg} alt="linkedin" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-4 col-sm-12">
+                        <div className={`card ${style.profileCard}`}>
+
+                            <img src={Ak} className={`card-img-top ${style.developerImage}`} alt="..." />
+
+                            <div className="card-body text-center">
+                                <h5 className="card-title">Aravind Krishnan</h5>
+                                <a href="https://www.linkedin.com/in/aravind-krishnan-808a85152/" target="_blank">
+                                <img src={LinkedIn} className={style.linkImg} alt="linkedin" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-4 col-sm-12">
+                        <div className={`card ${style.profileCard}`}>
+                            <img src={Jay} className={`card-img-top ${style.developerImage}`} alt="..." />
+                            <div className="card-body text-center">
+                                <h5 className="card-title">Jay Khatri</h5>
+                                <a href="https://www.linkedin.com/in/jay-khatri-a52483169/" target="_blank">
+                                <img src={LinkedIn} className={style.linkImg} alt="linkedin" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={`col-md-12 ${style.designerTitle}`}>
+                        <h1><span>Designers</span></h1>
+                    </div>
+
+                    <div className="col-md-2"></div>
+                    <div className="col-md-4 col-sm-12">
+                        <div className={`card ${style.profileCard}`}>
+                            <img src={Rahul} className={`card-img-top ${style.developerImage}`} alt="..." />
+                            <div className="card-body text-center">
+                                <h5 className="card-title">Rahul Bhide</h5>
+                                <a href="https://www.linkedin.com/in/rahul-bhide/" target="_blank">
+                                <img src={LinkedIn} className={style.linkImg} alt="linkedin" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-12">
+                        <div className={`card ${style.profileCard}`}>
+                            <img src={Rihan} className={`card-img-top ${style.developerImage}`} alt="..." />
+                            <div className="card-body text-center">
+                                <h5 className="card-title">Rihan Krithik Ramesh</h5>
+                                <a href="https://www.linkedin.com/in/rihankrithik/" target="_blank">
+                                <img src={LinkedIn} className={style.linkImg} alt="linkedin" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-2"></div>-
+                </div>
             </div>
         )
 
